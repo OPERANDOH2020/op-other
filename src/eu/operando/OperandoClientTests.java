abd896b36b7806ca41ed46b8b347e2694f72b17d
Matthew Gallagher <Matt.Gallagher@oxfordcc.co.uk>
2016-04-06 14:51:41 +0100

    add licences

--- src/eu/operando/OperandoClientTests.java
+++ src/eu/operando/OperandoClientTests.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2016 Oxford Computer Consultants Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The MIT License (MIT).
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *    Matthew Gallagher (Oxford Computer Consultants) - Creation.
+ * Initially developed in the context of OPERANDO EU project www.operando.eu
+ */
 package eu.operando;
 
 public class OperandoClientTests extends OperandoConstants
