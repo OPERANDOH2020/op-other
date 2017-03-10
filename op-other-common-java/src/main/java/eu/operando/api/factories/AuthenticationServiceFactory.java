b0ae0a96021823ff6ddaaf1d841dfdd38fa46fd8
William Frankish <william.frankish@oxfordcc.co.uk>
2017-03-10 14:33:57 +0000

    add temporary logging line

--- op-other-common-java/src/main/java/eu/operando/api/factories/AuthenticationServiceFactory.java
+++ op-other-common-java/src/main/java/eu/operando/api/factories/AuthenticationServiceFactory.java
@@ -1,5 +1,8 @@
 package eu.operando.api.factories;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import eu.operando.Utils;
 import eu.operando.api.AuthenticationService;
 import eu.operando.api.impl.AuthenticationServiceImpl;
@@ -10,8 +13,14 @@ public class AuthenticationServiceFactory {
 	private static final String PROPERTY_NAME_ORIGIN_AUTHENTICATION_API = "originAuthenticationApi";
 	
 	public static AuthenticationService getAuthenticationService(String propertyFileName){
+		
 		// Property file property values.
 		String originAuthenticationApi = Utils.loadPropertyString(propertyFileName, PROPERTY_NAME_ORIGIN_AUTHENTICATION_API);
+		
+		
+		//TODO remove when no longer needed
+		final Logger LOGGER = LogManager.getLogger(AuthenticationService.class);
+		LOGGER.info("AuthenticationService started with base url: ", originAuthenticationApi);
 		
 		// Create the clients based on the properties file.
 		ClientAuthenticationApiOperandoService clientAuthenticationService = 
