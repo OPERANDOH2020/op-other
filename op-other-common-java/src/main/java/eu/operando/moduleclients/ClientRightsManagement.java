32b48b2bf097ad219fbdb2be4a2131d094bb1d44
Matthew Gallagher <Matt.Gallagher@oxfordcc.co.uk>
2017-04-19 11:28:17 +0100

    Add RM client for GK

---
+++ op-other-common-java/src/main/java/eu/operando/moduleclients/ClientRightsManagement.java
@@ -0,0 +1,41 @@
+package eu.operando.moduleclients;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import eu.operando.OperandoAuthenticationException;
+
+public class ClientRightsManagement  extends ClientOperandoModule
+{
+	// Service ID for RM.
+	private static final String SERVICE_ID_RIGHTS_MANAGEMENT = "/operando/rm/";
+	
+	// Headers
+	private static final String HEADER_NAME_PSP_USER_IDENTIFIER = "psp-user-identifier";
+	private static final String HEADER_NAME_SERVICE_TICKET = "service-ticket";
+
+	private ClientAuthenticationApiOperandoClient clientAuthenticationApi = null; //TODO - abstract to a superclass?
+
+	public ClientRightsManagement(String originRightsManagement, ClientAuthenticationApiOperandoClient clientAuthenticationService)
+	{
+		super(originRightsManagement, true);
+		this.clientAuthenticationApi  = clientAuthenticationService;
+	}
+
+	public Response sendRequest(String httpMethod, MultivaluedMap<String, String> headersToDan, String idOspUser, String pathPlus, MultivaluedMap<String, String> queryParameters, String body) throws OperandoAuthenticationException
+	{
+		String serviceTicket = requestServiceTicket(SERVICE_ID_RIGHTS_MANAGEMENT);
+		
+		headersToDan.add(HEADER_NAME_SERVICE_TICKET, serviceTicket);
+		headersToDan.add(HEADER_NAME_PSP_USER_IDENTIFIER, idOspUser);
+		
+		String endpoint = "/" + pathPlus;
+		return sendRequest(httpMethod, endpoint, headersToDan, body, queryParameters);
+	}
+
+	private String requestServiceTicket(String serviceId) throws OperandoAuthenticationException
+	{
+		return clientAuthenticationApi.requestServiceTicket(serviceId);
+	}	
+}
+
