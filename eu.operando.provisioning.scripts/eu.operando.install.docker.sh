8bc5a80fa23d623d8290df976fdf017a6b55ea7c
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-04-24 15:36:08 +0200

    scripts to facilitate platform start and stop

---
+++ eu.operando.provisioning.scripts/eu.operando.install.docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#check ubuntu 16.04
+OS_TYPE=`cat /etc/issue | awk -F. '{printf($1)}'`
+OS_VERSION=`cat /etc/issue | awk -F. '{printf($2)}'`
+if [[ $OS_TYPE -eq "Ubuntu" ] && [ $OS_VERSION -eq "16.04.2" ]]
+then
+  echo "Same OS and Version"
+else
+  echo "Warning: This script was developed for Ubuntu 16.04"
+fi
+
+sudo apt-get update
+# sudo apt-get -y upgrade
+
+# add Docker repo
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+
+# for uafs storage driver (not sure if really needed but in case of)
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+#install docker
+sudo apt-get install -y docker-engine
+
+# not really needed start on default
+# sudo service docker start
+
+
