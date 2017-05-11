10f59a9bba426d7eea91a0a3507a7ac7f212a40a
William Frankish <william.frankish@oxfordcc.co.uk>
2017-05-11 14:48:47 +0100

    OFUSE-366 more fixes to curl commands

--- eu.operando.provisioning.scripts/eu.operando.occ.configure.sh
+++ eu.operando.provisioning.scripts/eu.operando.occ.configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d @eu.operando.occ.configure.amiuser.json "localhost:8080/operando/interfaces/aapi/aapi/user/register"
+curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d @eu.operando.occ.configure.amiuser.json "localhost:8135/operando/interfaces/aapi/aapi/user/register"
 
-curl -X POST --header "Content-Type: application/json" -- header "Accept: */*" -d @eu.operando.occ.configure.amipolicies.json "localhost:8096/operando/core/pdb/OSP"
+curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d @eu.operando.occ.configure.amipolicies.json "localhost:8096/operando/core/pdb/OSP"
