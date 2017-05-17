619edf090ca893a1231e722f9eae888cadd7dd73
William Frankish <william.frankish@oxfordcc.co.uk>
2017-05-17 15:27:49 +0100

    Amirando setup python - post new policy

--- eu.operando.provisioning.scripts/occ-amiaccountsetup/amiAccountSetup.py
+++ eu.operando.provisioning.scripts/occ-amiaccountsetup/amiAccountSetup.py
@@ -60,8 +60,35 @@ def get_default_user_privacy_policy():
     else:
         raise OperandoException("Failed to get default UPPs. Returned osp_policies were" + str(osp_policies))
 
+def create_privacy_policy_dict(username, preference):
+    data = {}
+    data["user_id"] = username;
+    data["user_preferences"] = []
+    data["subscribed_osp_policies"] = []
+    policy = {}
+    policy["osp_id"] = config.osp_id
+    policy["access_policies"] = preference
+    data["subscribed_osp_policies"].append(policy)
+    data["subscribed_osp_settings"] = []
+    return data
+
+def log_post_privacy_policy(username, response):
+    print response
+    if response.status_code:
+        print response.status_code
+        success = response.status_code == 201
+    else:
+        success = False
+    if success:
+        print "succesfully posted policy for user with name {0}".format(username)
+    else:
+        print "failed to post policy for {0}".format(username)
+
 def post_user_privacy_policy(username, preference):
-    return ""
+    url = config.create_user_privacy_policy_url;
+    data = create_privacy_policy_dict(username, preference)
+    response = requests.post(url, json=data, timeout=config.timeout)
+    log_post_privacy_policy(username, response)
 
 ### this is the code that runs when you run this file ###
 print "Creating user logins"
--- eu.operando.provisioning.scripts/occ-amiaccountsetup/config.py
+++ eu.operando.provisioning.scripts/occ-amiaccountsetup/config.py
@@ -1,9 +1,9 @@
 # generic config
-root_url = "http://192.168.99.177"
+root_url = "http://localhost"
 timeout = 20
 # create account config
 create_user_url = root_url + ":8135/operando/interfaces/aapi/aapi/user/register"
 # create preferences config
 get_osp_policies_url = root_url + ":8096/operando/core/pdb/OSP/?filter="
-create_user_privacy_policy_url = root_url + ":8096/operando/core/pdb/user_privacy_policy"
+create_user_privacy_policy_url = root_url + ":8096/operando/core/pdb/user_privacy_policy/"
 osp_id = "Ami"
