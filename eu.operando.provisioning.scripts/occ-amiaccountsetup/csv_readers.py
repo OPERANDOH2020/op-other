33f0d7d19d8ebb62c0a60f0211ee0656f7724f34
Matthew Gallagher <Matt.Gallagher@oxfordcc.co.uk>
2017-06-15 10:08:49 +0100

    OFUSE-391 refactor csv parsing

--- eu.operando.provisioning.scripts/occ-amiaccountsetup/csv_readers.py
+++ eu.operando.provisioning.scripts/occ-amiaccountsetup/csv_readers.py
@@ -16,20 +16,25 @@ class DataReader(object):
         with open(self.csv_path, "r") as csv_file:
             reader = csv.DictReader(csv_file)
             for line in reader:
-                index = int(line[DataReader.indexkey])
-                if(line[DataReader.accountkey] == '1'):
-                    # only bother giving roles to people who have ami accounts
-                    type = line[DataReader.typekey]
-                    if(type == "VL"):
-                        role = "volunteer_linkup"
-                    elif (type == "AGNS"):
-                        role = "abingdon_good_neigbour_scheme"
-                    elif(type == "Volunteer"):
-                        role = "Volunteer"
-                    else:
-                        role = ""
-                else:
-                    role = ""
-                password = user.create_password(index, role)
-                users.append(User(index, password, role))
+                user_from_line = self.read_user_from_line(line)
+                users.append(user_from_line)
         return users
+
+    def read_user_from_line(self, line):
+        index = int(line[DataReader.indexkey])
+        role = self.read_user_role_from_line(line)
+        password = user.create_password(index, role)
+        return User(index, password, role)
+
+    def read_user_role_from_line(self, line):
+        role = ""
+        has_operando_account = line[DataReader.accountkey] == '1'
+        if(has_operando_account):
+            type = line[DataReader.typekey]
+            if(type == "VL"):
+                role = "volunteer_linkup"
+            elif (type == "AGNS"):
+                role = "abingdon_good_neigbour_scheme"
+            elif(type == "Volunteer"):
+                role = "Volunteer"
+        return role
